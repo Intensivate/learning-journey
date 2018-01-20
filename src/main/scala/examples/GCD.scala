2be7f81ab723c6f1601ea8d21000a5ffcc1ae253
apaj <aleksandar.pajkanovic@gmail.com>
2018-01-20 13:49:07 +0100

    Adapting GCD to Learning Journey

--- src/main/scala/examples/GCD.scala
+++ src/main/scala/examples/GCD.scala
@@ -1,25 +1,22 @@
 // See LICENSE.txt for license details.
+// January 20th, 2018	- adapted to Learning Journey
 package examples
 
-import chisel3._
-
-class GCD extends Module {
-  val io = IO(new Bundle {
-    val a  = Input(UInt(16.W))
-    val b  = Input(UInt(16.W))
-    val e  = Input(Bool())
-    val z  = Output(UInt(16.W))
-    val v  = Output(Bool())
-  })
-
-  val x  = Reg(UInt())
-  val y  = Reg(UInt())
-  when   (x > y) {
-    x := x - y
-  }
-    .elsewhen (x <= y) { y := y - x }
-
-  when (io.e) { x := io.a; y := io.b }
-  io.z := x
-  io.v := y === 0.U
+import Chisel._ 
+ 
+class GCD extends Module { 
+  val io = new Bundle { 
+    val a  = UInt(INPUT,  16) 
+    val b  = UInt(INPUT,  16) 
+    val e  = Bool(INPUT) 
+    val z  = UInt(OUTPUT, 16) 
+    val v  = Bool(OUTPUT) 
+  } 
+  val x  = Reg(UInt()) 
+  val y  = Reg(UInt()) 
+  when   (x > y) { x := x - y } 
+  unless (x > y) { y := y - x } 
+  when (io.e) { x := io.a; y := io.b } 
+  io.z := x 
+  io.v := y === UInt(0) 
 }
